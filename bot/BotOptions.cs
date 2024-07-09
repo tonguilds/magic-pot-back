08329f31da014c72f56f31e0a7985e5441f8bcc1
Dmitry Popov <dmitry@parsec-club.ru>
2024-07-09 19:31:50 +0300

    breaking: bot removed

--- bot/BotOptions.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace MagicPot.Bot
-{
-    public class BotOptions
-    {
-        public string Token { get; set; } = string.Empty;
-
-        public string WebhookUrl { get; set; } = string.Empty;
-    }
-}
--- publish-bot.sh
+++
@@ -1,5 +0,0 @@
-dotnet build bot/bot.csproj -c Release && \
-  sudo systemctl stop magicpot-bot && \
-  sudo dotnet publish bot/bot.csproj -c Release -o /var/www/magicpot-bot && \
-  sudo systemctl start magicpot-bot && \
-  journalctl -fu magicpot-bot -n 10 --no-hostname
